f7e78e246b5d54a2041a9f746810361c28788ba4
Guilherme Keunecke Bangemann <guilherme.bangemann@universo.univates.br>
2021-12-15 21:24:57 -0300

    Final Project Done

---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	public int points = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/*public void OnTriggerEnter(Collider points)
+	{
+		if (points.gameObject.tag == "Coins") {
+			Debug.Log ("Moeda coletada!");
+			points = points + 1;
+			Destroy(points.gameObject);
+		}
+	}*/
+
+	private void OnGUI()
+	{
+		GUI.Label (new Rect(10, 10, 100, 20), "Pontuação: " + points);		
+	}
+}
---
+++ Assets/scripts/TeleportingScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportingScript : MonoBehaviour {
+
+	public Transform teleportTarget;
+	public GameObject thePlayer;
+
+	void OnTriggerEnter(Collider other)
+	{
+		thePlayer.transform.position = teleportTarget.transform.position;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
